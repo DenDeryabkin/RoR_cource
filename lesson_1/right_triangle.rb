e6707fdad0367c3b6989a5c163a08b42b5c9d186
Your Name <your_email@whatever.com>
2022-11-30 18:24:09 +0300

    finish test 3 righth_triangel
    залил правильную версию решения задачи.

--- lesson_1/right_triangle.rb
+++ lesson_1/right_triangle.rb
@@ -1,8 +1,15 @@
 print "введите сторону (А): "
 side_a = gets.chomp
-
 print "введите сторону (B): "
 side_b = gets.chomp
-
 print "введите сторону (C): "
 side_c = gets.chomp
+
+if side_a == side_b && side_a == side_c
+  puts "треугольник равнобедренный и равносторонний"
+elsif (side_a == side_b && side_a != side_c) || (side_a == side_c && side_a != side_b) || (side_b == side_c && side_b != side_a)
+  puts "Треугольник равнобедренный"
+elsif (side_a**2 + side_b**2) == side_c**2 || (side_a**2 + side_c**2) == side_b**2 || (side_c**2 + side_b**2) == side_a**2
+  puts "Треугольник ровный"
+  else
+end
